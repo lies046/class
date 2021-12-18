39265b2735695b9feca90660f7be4787852f5b11
Shohei Ito <skytree.skysky@gmail.com>
2021-12-18 15:17:01 +0900

    getset

--- basic.php
+++ basic.php
@@ -2,11 +2,18 @@
 
 require_once 'Person.php';
 require_once 'Area.php';
+require_once 'TriangleFigure.php';
 
 $p = new Person('山田','太郎');
+$tri = new TriangleFigure();
 
+$tri->setBase(10);
+$tri->setHeight(10);
+print $tri->getArea();
+echo '<br>';
 $p->show();
 echo '<br>';
 print Area::PI;
 echo '<br>';
 print '円の面積:'.Area::circle(10).'cm^2';
+echo '<br>';
