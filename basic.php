65404b16f6d6e59ad73d80543e142660e823b276
Shohei Ito <skytree.skysky@gmail.com>
2021-12-18 14:41:52 +0900

    コンストラクタ

--- basic.php
+++ basic.php
@@ -2,8 +2,6 @@
 
 require_once 'person1.php';
 
-$p = new Person();
-$p->firstName = '山田';
-$p->lastName = '太郎';
+$p = new Person('山田','太郎');
 
 $p->show();
--- person1.php
+++ person1.php
@@ -5,6 +5,14 @@ class person
     public $firstName;
     public $lastName;
 
+    public function __construct(string $firstName, string $lastName)
+    {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+    }
+
+
+
     public function show()
     {
         print $this->firstName . $this->lastName;
