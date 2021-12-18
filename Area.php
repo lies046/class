d5ab62b93e420ff4fd0a9d6c43b71a786c99f49c
Shohei Ito <skytree.skysky@gmail.com>
2021-12-18 14:57:33 +0900

    クラス定数

--- Area.php
+++ Area.php
@@ -2,9 +2,9 @@
 
 class Area
 {
-    public static $pi= 3.14;
+    const PI = 3.14;
     public static function circle(float $radius): float
     {
-        return pow($radius, 2) * self::$pi ;
+        return pow($radius, 2) * self::PI ;
     }
 }
--- basic.php
+++ basic.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once 'person1.php';
+require_once 'Person.php';
 require_once 'Area.php';
 
 $p = new Person('山田','太郎');
 
 $p->show();
 echo '<br>';
-print Area::$pi;
+print Area::PI;
 echo '<br>';
 print '円の面積:'.Area::circle(10).'cm^2';
