0ae422272ea5e3e265a981b6b489a1c3494e9cde
Shohei Ito <skytree.skysky@gmail.com>
2021-12-19 14:44:37 +0900

    継承

---
+++ BusinessPerson.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'Person.php';
+
+class BusinessPerson extends Person
+{
+  public function work()
+  {
+      print $this->firstName . $this->lastName . 'は働いています';
+  }
+}
--- basic.php
+++ basic.php
@@ -3,10 +3,11 @@
 require_once 'Person.php';
 require_once 'Area.php';
 require_once 'TriangleFigure.php';
+require_once 'BusinessPerson.php';
 
 $p = new Person('山田','太郎');
 $tri = new TriangleFigure();
-
+$bp = new BusinessPerson('山田','一郎');
 $tri->setBase(10);
 $tri->setHeight(10);
 print $tri->getArea();
@@ -17,3 +18,4 @@ print Area::PI;
 echo '<br>';
 print '円の面積:'.Area::circle(10).'cm^2';
 echo '<br>';
+print $bp->work();
