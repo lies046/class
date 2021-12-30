2028a4cc194cff0f30340db29ae395d4a06d5023
Shohei Ito <skytree.skysky@gmail.com>
2021-12-30 23:23:36 +0900

    確認

--- test.php
+++ test.php
@@ -1,3 +1,12 @@
 <?php
 
-$data = ['aaa','bbb','ccc','ddd'];
+$data = [
+    ['aaa', 'AAA'],
+    ['bbb', 'BBB'],
+    ['ccc', 'CCC']
+];
+
+foreach ($data as [$a, $B]){
+    var_dump($a);
+    var_dump($B);
+}
